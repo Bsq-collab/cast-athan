5f6475eec668d49a3681bd12fa10cbe013786eaa
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2020-06-26 04:01:56 -0400

    Now updates prayer times at midnight

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 const ChromecastAPI = require("chromecast-api");
 
+const getMidnight = () => {
+	const msInDay = 60 * 60 * 24 * 1000;
+	const tomorrowDate = new Date( new Date().getTime() + msInDay );
+
+	tomorrowDate.setHours(0);
+	tomorrowDate.setMinutes(0);
+	tomorrowDate.setSeconds(0);
+	return tomorrowDate;
+}
+
 const convertToDates = obj => {
-  for (key in obj) {
-    const timeArray = obj[key]
-      .split(":")
-      .map((i) => Number(i));
-    const timeDate = new Date();
-    timeDate.setHours(timeArray[0]);
-    timeDate.setMinutes(timeArray[1]);
-    obj[key] = timeDate;
-  }
+	for (key in obj) {
+		const timeArray = obj[key]
+		.split(":")
+		.map((i) => Number(i));
+		
+		const timeDate = new Date();
+		timeDate.setHours(timeArray[0]);
+		timeDate.setMinutes(timeArray[1]);
+		obj[key] = timeDate;
+	}
 }
 
 let timings = {
 	// Fajr: "03:45",
 	// Sunrise: '05:25',
 	// Dhuhr: "12:56",
-	Asr: "16:59",
+	//Asr: "16:59",
 	// Sunset: '20:31',
-	Maghrib: "20:31",
-	Isha: "22:11",
+	//Maghrib: "20:31",
+	//Isha: "22:11",
 	// Imsak: '03:35',
 	// Midnight: '00:58'
 };
@@ -29,7 +40,7 @@ convertToDates(timings);
 const axios = require("axios");
 
 const getTimes = () => {
-	axios
+	return axios
 		.get(
 			"http://api.aladhan.com/v1/timingsByCity?city=New+York&country=United+States&method=2"
 		)
@@ -41,7 +52,14 @@ const getTimes = () => {
 			delete newTimings.Midnight;
 			timings = newTimings;
 
-      convertToDates(timings);
+      			convertToDates(timings);
+		})
+		.catch(console.log)
+		.finally(() => {
+			setTimeout(
+				getTimes, 
+				getMidnight().getTime() - new Date().getTime()
+			);
 		});
 };
 
@@ -50,13 +68,13 @@ const playAdhan = (isFajr = false) => {
 
 	client.on("device", function (device) {
 		// if (device.friendlyName == "1st Floor speaker") {
-			var mediaURL = isFajr ?
-      "https://res.cloudinary.com/ddakrweyq/video/upload/v1592809375/fajr_adhan_ptbjhq.mp3" :
-      "https://res.cloudinary.com/ddakrweyq/video/upload/v1592860533/videoplayback_1_jndba1.mp4";
+			const mediaURL = isFajr ?
+      				"https://res.cloudinary.com/ddakrweyq/video/upload/v1592809375/fajr_adhan_ptbjhq.mp3" :
+     				"https://res.cloudinary.com/ddakrweyq/video/upload/v1592860533/videoplayback_1_jndba1.mp4";
 
 			device.play(mediaURL, function (err) {
 				if (err) {
-          console.log(err);
+          				console.log(err);
 				}
 			});
 		// }
@@ -71,7 +89,8 @@ const checkTime = () => {
 			delete timings[key];
 		}
 	}
+	
 };
 
-setInterval(getTimes, 86400 * 1000);
-setInterval(checkTime, 30 * 1000);
+setInterval(checkTime, 5 * 1000);
+getTimes();
